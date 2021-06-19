b4e636767e3937e9ab54c48b5d2c9498276394f8
Walter <walterbivanco@gmail.com>
2021-06-19 10:23:20 -0300

    Se hicieron los cometarios iniciales en las vistas y en el archivo services de la app api

--- apps/api/services.py
+++ apps/api/services.py
@@ -1,6 +1,12 @@
 import requests
 
+
 def generate_request(url, params={}):
+    """
+    Obtiene la respuesta de la url de los parametros que se recibe la función,  verifica si la misma es correcta y si es
+    así devuelve la misma en un json.
+    """
+
     response = requests.get(url, params=params)
 
     if response.status_code == 200:
@@ -8,16 +14,24 @@ def generate_request(url, params={}):
 
 
 def get_sellers_by_category(params={}):
+    """
+    Obtiene el listado de los vendores.
+    """
+
     response = generate_request('https://api.mercadolibre.com/categories/MLA352679', params)
 
     if response:
-       return response
+        return response
     return ''
 
 
 def get_publishings_more_expensive(params={}):
+    """
+    Obtiene el listado de las publicaciones.
+    """
+
     response = generate_request('https://api.mercadolibre.com/sites/MLA/search?category=MLA352679', params)
 
     if response:
-       return response
+        return response
     return ''
--- apps/api/views.py
+++ apps/api/views.py
@@ -4,12 +4,20 @@ from .services import get_sellers_by_category, get_publishings_more_expensive
 
 
 def home(request):
+    """
+    Muestra ver la pantalla de inicio con el menú principal.
+    """
+
     tenmplate_name = "home.html"
 
     return render(request, tenmplate_name, {})
 
 
 def ranking_publishings(request):
+    """
+    Muestra el listado de los 5 vendedores que más vendieron en la categoría MLA352679
+    """
+
     template_name = "publishings_list.html"
 
     params = { }
@@ -24,6 +32,10 @@ def ranking_publishings(request):
 
 
 def ranking_sellers(request):
+    """
+    Muestra el listado de la 20 punlicaciones más caras en la categoría MLA352679
+    """
+
     template_name = "sellers_list.html"
 
     params = { }
